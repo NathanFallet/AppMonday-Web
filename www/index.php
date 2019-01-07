d33b789b1982b75cbc27500f72b3f6a1a60a4e61
Nathan Fallet <contact@nathanfallet.me>
2019-01-07 23:40:00 +0100

    Adding google site verification

--- www/index.php
+++ www/index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Every Monday, we will share one app you submitted here in our Instagram story. Fill this form and share with us your project! It can be a website, a mobile app, an open source project, ... everything you coded.">
     <meta name="author" content="Nathan Fallet">
+    <meta name="google-site-verification" content="CqkRiFjAXU4WD4enP7cenkQruEuOiOJ0Sckj_zknNZc">
     <link rel="apple-touch-icon" href="assets/images/AppMondayRadius.png">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <!-- FontAwesome JS -->
