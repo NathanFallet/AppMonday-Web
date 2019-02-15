6aa7e449e4531201e3c66a369a97f62fd05de2b9
Nathan Fallet <contact@nathanfallet.me>
2019-02-15 18:23:21 +0100

    Availability check

---
+++ api/config/check.php
@@ -0,0 +1,14 @@
+<?php
+echo json_encode(array(
+  'iOS' => array(
+    'projects' => false,
+    'competitions' => true,
+    'tips' => true
+  ),
+  'android' => array(
+    'projects' => true,
+    'competitions' => true,
+    'tips' => true
+  )
+));
+?>
