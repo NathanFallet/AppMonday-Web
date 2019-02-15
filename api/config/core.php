9d244fa49f4ed1040582b2376ba8fbc1838bbec2
Nathan Fallet <contact@nathanfallet.me>
2019-02-15 18:42:52 +0100

    Update to be better

--- api/config/core.php
+++ api/config/core.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__).'/../../../config/config.php';
 // Headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Token, username, password");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, client");
 
 // Generate utils methods
 
@@ -13,11 +13,39 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 // getDBHost(), getDBUsername(), getDBPassword()
 
 // Read data from request
+$headers = apache_request_headers();
 $method = $_SERVER["REQUEST_METHOD"];
 if($method == 'OPTIONS'){
   exit;
 }else if($method == 'POST' || $method == 'PUT'){
   $data = json_decode(file_get_contents('php://input'), true);
+}
+
+// Availability check
+$check = array(
+  'AppMonday_iOS' => array(
+    'project' => false,
+    'competition' => true,
+    'tip' => true
+  ),
+  'AppMonday_Android' => array(
+    'project' => true,
+    'competition' => true,
+    'tip' => true
+  )
+);
+
+$folder = '';
+$paths = explode('/', $_SERVER['REQUEST_URI']);
+for($i = 0; $i < $paths && empty($folder); $i++) {
+  $folder = $paths[$i];
+}
+if(isset($headers['client']) && isset($check[$headers['client']]) && isset($check[$headers['client']][$folder])) {
+  $available = $check[$headers['client']][$folder];
+  if(!$available) {
+    echo json_encode(array());
+    exit;
+  }
 }
 
 // Connect to database
